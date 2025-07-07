5df1f2194824a5d9d4c61ac7db13e87861b70b08
ASC <ASC@Rahul>
2025-07-07 22:43:25 +0530

    create hibernate  first project

---
+++ src/main/resources/snippet/Snippet.java
@@ -0,0 +1,11 @@
+package snippet;
+
+public class Snippet {
+	<property name="hibernate.dialect">org.hibernate.dialect.MySQLDialect</property>
+			<property name="hibernate.show_sql">true</property>
+			<property name="hibernate.format_sql">true</property>
+	
+			<property name="hibernate.hbm2ddl.auto">update</property>
+	
+}
+
